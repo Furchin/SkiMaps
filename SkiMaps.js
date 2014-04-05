f357d1f0f0ea8b09a02f88a452cb9f4bfbef0688
Michal Bryc <furchin@gmail.com>
2014-04-05 12:02:17 -0700

    Initial meteor framework; very early and not a lot of functionality.

---
+++ SkiMaps.js
@@ -0,0 +1,61 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to SkiMaps.";
+  };
+
+  Meteor.startup( function() {
+    console.log('hi');
+    //filepicker.setKey("AlCrL69bBRFOX9cgnAJ0wz"); 
+    //filepicker.constructWidget(document.getElementById('attachment'));
+  });
+
+  Template.template.events({
+    'change #attachment': function(evt){
+        console.log(evt.files);
+    }
+});
+
+/*
+  filepicker.pick({
+      mimetypes: ['image/*', 'text/plain'],
+      container: 'window',
+      services:['COMPUTER', 'FACEBOOK', 'GMAIL'],
+    },
+    function(InkBlob){
+      console.log(JSON.stringify(InkBlob));
+    },
+    function(FPError){
+      console.log(FPError.toString());
+    }
+  );
+
+  Templates.template.events({
+    'change #attachment': function(evt){
+      console.log(evt.files);
+    }
+  });
+
+  */
+  /*
+  // Hook up GPX Button
+  $('#loadBtn').click(function() {
+    $("#file_upload").click();
+  });
+
+  $('input[type=file]').change(function() { 
+    
+  });
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  //L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+      attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
+      maxZoom: 18
+  }).addTo(map);
+  */
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
